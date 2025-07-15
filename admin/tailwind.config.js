c9d846992b64f1a0a3d4212b32f364dc16eaf512
SATYAM KUMAR SINGH <126961932+satyamrajput-7913@users.noreply.github.com>
2025-07-15 22:29:46 +0530

    Add files via upload

---
+++ admin/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,html}"
+  ],
+  theme: {
+    extend: {
+        colors:{
+            'primary' : "#5F6FFF"
+        }
+    },
+  },
+  plugins: [],
+}
